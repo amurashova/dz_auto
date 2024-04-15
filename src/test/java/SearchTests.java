bef130bd67803b135b19e980764b7ac909dd816b
anastasiamurasova <nomofamilinomo@gmail.com>
2024-04-15 17:15:47 +0300

    second try

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,14 +1,10 @@
-import org.junit.jupiter.api.Test;
+package guru.qa;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+public class Main {
 
-public class SearchTests {
-    @Test
-    void successfulSearchTest() {
-        open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+    private static String msg = "Hello qa.guru!";
+
+    public static void main(String... args) {
+        System.out.println(msg);
     }
 }
